6d4484102d00f3c330be40342c4497505a06dace
Yeshwanth Kanukuntla <yeshwanth0068@gmail.com>
2025-04-17 06:01:31 -0500

    New changes in path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/DevOps/", // example: /devops-portfolio/
+  base: "/devops/", // example: /devops-portfolio/
 })
